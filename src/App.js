0d7b1d70cd3b73d2ea2ca65b5443dc5ed390c0e6
descalante7 <daniel.esc11@gmail.com>
2022-12-13 09:56:16 -0500

    initial commit.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
+import logo from './assets/Miami-Heat-logo.png';
 import './App.css';
 
 function App() {
@@ -6,17 +7,26 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <div className="d-grid gap-2">
+        <br />
+          <Button variant="danger" size="lg">
+            League & Player Information
+          </Button>
+          <Button variant="danger" size="lg">
+            Manage Scouting Reports
+          </Button>
+        </div>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
