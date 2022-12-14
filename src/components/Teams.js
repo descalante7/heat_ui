0725530586c44f134e2e22d03dc62989105aa86c
descalante7 <daniel.esc11@gmail.com>
2022-12-13 22:29:33 -0500

    added filtering to teams component

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import {Link, useHistory, withRouter, useParams} from 'react-router-dom';
@@ -16,19 +16,37 @@ class Teams extends Component {
             teamData: [],
             playerData: [],
             leagueKey: this.props.leagueKey,
-            move: false
+            move: false,
+            name: '',
+            listPlayers: false
         }
-    }     
+    }
+
+    searchHandler = (e) => {   
+        e.preventDefault();
+        if(e.target.value.length > 1) {
+            this.setState({listPlayers: true});
+        }else {
+            this.setState({listPlayers: false});
+        }
+        this.setState({name: e.target.value});
+    }
    
     componentDidMount = async () => {
         axios.get(config.BaseUrl + 'teams?leagueKey=' + this.state.leagueKey)
-            .then(res => {
-                console.log(res);
-                this.setState({teamData: res.data});
-            })          
-            .catch(err => {
-                console.log(err);
-            });
+        .then(res => {
+            console.log(res);
+            this.setState({teamData: res.data});
+            let yr = new Date().getFullYear();
+            return axios.get(config.BaseUrl + 'Players?seasonKey=' + yr)
+        })
+        .then(res =>{
+            console.log(res);
+            this.setState({playerData: res.data});
+        })       
+        .catch(err => {
+            console.log(err);
+        });
     }
 
     playersFlag = async (teamKey) => {
@@ -39,12 +57,29 @@ class Teams extends Component {
     }   
 
     render() {
+        let list = this.state.playerData
+            .filter(d => this.state.name === '' || d.firstName.toLowerCase().includes(this.state.name.toLowerCase()));              
+
+             //.map((d, index) => <li key={index}>{d}</li>);
+
         return (
             <div className="App">
                 <header className="App-header">
                     <div className="d-grid gap-2">
                     <br />           
                     <a href="http://localhost:3000"><img src={logo}  alt="logo" /></a>
+                    <h3>Current Player Search</h3>
+                    <form>
+                        <input type="search" name="player" value={this.state.name} onChange={this.searchHandler} />                       
+                    </form>
+                    <ul>                    
+                    {this.state.listPlayers && this.state.playerData && list.map((player) => 
+                            <li>{player.firstName} {player.lastName}</li>
+                    )}
+                    </ul>
+                    
+                    <br />
+                    <h3>Player Search by Team</h3>
                     {this.state.teamData && this.state.teamData.map((team) =>
                         <Button variant="danger" size="lg" key={team.teamKey} value={team.teamKey} onClick={() =>this.playersFlag('/players')}>{team.teamKey}  {team.teamName}</Button>
                     )}
